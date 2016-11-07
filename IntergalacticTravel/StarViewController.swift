6da7e41a177aad305514d2565bf231cf4410d883
Logan <lboven7038@stu.d214.org>
2016-11-07 10:13:26 -0600

    Added new view controller and added segues.

---
+++ IntergalacticTravel/StarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StarViewController.swift
+//  IntergalacticTravel
+//
+//  Created by student3 on 11/3/16.
+//  Copyright © 2016 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class StarViewController: UIViewController {
+
+    @IBOutlet weak var starImageView: UIImageView!
+    var isBlueStar: Bool?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if isBlueStar!
+        {
+            view.backgroundColor = UIColor.blue
+            starImageView.image = UIImage(named: "BlueStar")
+        }
+        else
+        {
+                view.backgroundColor = UIColor.red
+            starImageView.image = UIImage(named: "RedDwarf")
+        }
+    }
+}
